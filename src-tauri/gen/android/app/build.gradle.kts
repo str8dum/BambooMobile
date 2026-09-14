7e6d169b83dfee4b7c472b2a61a2c7b56d98b091
str8dum <str8dum@yahoo.com>
2026-09-14 18:13:53 -0400

    Give X2D.2 test build a unique Android package ID

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -26,11 +26,13 @@ android {
     }
     buildTypes {
         getByName("debug") {
+            applicationIdSuffix = ".x2d2"
             manifestPlaceholders["usesCleartextTraffic"] = "true"
             isDebuggable = true
             isJniDebuggable = true
             isMinifyEnabled = false
-            packaging {                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
+            packaging {
+                jniLibs.keepDebugSymbols.add("*/arm64-v8a/*.so")
                 jniLibs.keepDebugSymbols.add("*/armeabi-v7a/*.so")
                 jniLibs.keepDebugSymbols.add("*/x86/*.so")
                 jniLibs.keepDebugSymbols.add("*/x86_64/*.so")
